5bab3fdc4f1a8671fe061102cbe7dcfdfb73ce76
Arun Atluri <atluriarun@gmail.com>
2019-05-28 19:04:21 +1200

    Added and Unit tested Order Creation and Checkout UI's.

--- MVCSite/MyShop.Core/Infrastructure/Abstract/IBasketService.cs
+++ MVCSite/MyShop.Core/Infrastructure/Abstract/IBasketService.cs
@@ -14,5 +14,6 @@ namespace MyShop.Core.Infrastructure.Abstract
         void RemoveFromBasket(HttpContextBase httpContext, string itemId);
         List<BasketItemViewModel> GetBasketItems(HttpContextBase httpContext);
         BasketSummaryViewModel GetBasketSummary(HttpContextBase httpContext);
+        void ClearBasket(HttpContextBase httpContext);
     }
 }
---
+++ MVCSite/MyShop.Services/OrderService.cs
@@ -0,0 +1,39 @@
+﻿using MyShop.Core.Infrastructure.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
+
+namespace MyShop.Services
+{
+    public class OrderService : IOrderService
+    {
+        IRepository<Order> orderContext;
+
+        public OrderService(IRepository<Order> OrderContext)
+        {
+            this.orderContext = OrderContext;
+        }
+
+
+        public void CreateOrder(Order baseOrder, List<BasketItemViewModel> basketItems)
+        {
+            foreach(var item in basketItems)
+            {
+                baseOrder.OrderItems.Add(new OrderItem() {
+                    ProductId = item.Id,
+                    ProductName = item.ProductName,
+                    Image = item.Image,
+                    Quantity = item.Quantity,
+                    Price = item.Price
+                });
+            }
+
+            orderContext.Insert(baseOrder);
+            orderContext.Commit();
+        }
+    }
+}
